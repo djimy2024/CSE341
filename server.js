95185aa23713a1a18fa76939ff7386dc4cd233ea
Francillon2024 <fra24017@byui.edu>
2025-07-05 17:09:42 -0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 app.listen( port);
