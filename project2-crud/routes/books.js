6dfd63335a426e23651023640f698046d26d1564
Francillon2024 <fra24017@byui.edu>
2025-07-24 15:07:55 -0400

    update routes

--- project2-crud/routes/books.js
+++ project2-crud/routes/books.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/books');
 const { body } = require('express-validator');
+const {isAuthenticate} = require('../middleware/authenticate');
 
 router.get('/', booksController.getAll);
 router.get('/:id', booksController.getSingle);
-router.post('/', booksController.createBook);
-router.put('/:id', booksController.updateBook);
-router.delete('/:id', booksController.deleteBook);
+router.post('/', isAuthenticate, booksController.createBook);
+router.put('/:id', isAuthenticate, booksController.updateBook);
+router.delete('/:id', isAuthenticate, booksController.deleteBook);
 
 module.exports = router;
--- project2-crud/routes/index.js
+++ project2-crud/routes/index.js
@@ -1,3 +1,4 @@
+const passport = require('passport');
 const router = require('express').Router();
 router.use('/', require('./swagger'));
 
@@ -8,6 +9,15 @@ router.get('/', (req, res) => {
 router.use('/books', require('./books'));
 
 router.use('/authors', require('./authors'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err){
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
 
